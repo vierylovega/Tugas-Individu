85ddd257385abc5f1c42721d553454796a1f2d6b
vierylovega <62126637+vierylovega@users.noreply.github.com>
2020-05-22 17:48:06 +0700

    Adding Individual Task

---
+++ Tugas Kalkulator/Kalkulator1/Form1.Designer.vb
@@ -0,0 +1,303 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.B7 = New System.Windows.Forms.Button()
+        Me.B8 = New System.Windows.Forms.Button()
+        Me.Bdel = New System.Windows.Forms.Button()
+        Me.B9 = New System.Windows.Forms.Button()
+        Me.B5 = New System.Windows.Forms.Button()
+        Me.B4 = New System.Windows.Forms.Button()
+        Me.Bx = New System.Windows.Forms.Button()
+        Me.B6 = New System.Windows.Forms.Button()
+        Me.B2 = New System.Windows.Forms.Button()
+        Me.B1 = New System.Windows.Forms.Button()
+        Me.Bbagi = New System.Windows.Forms.Button()
+        Me.B3 = New System.Windows.Forms.Button()
+        Me.Bkurang = New System.Windows.Forms.Button()
+        Me.Btambah = New System.Windows.Forms.Button()
+        Me.Bsamadengan = New System.Windows.Forms.Button()
+        Me.B0 = New System.Windows.Forms.Button()
+        Me.BKeluar = New System.Windows.Forms.Button()
+        Me.BPlusminus = New System.Windows.Forms.Button()
+        Me.Bkoma = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 22.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(30, 40)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(388, 49)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'B7
+        '
+        Me.B7.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.B7.Location = New System.Drawing.Point(31, 108)
+        Me.B7.Name = "B7"
+        Me.B7.Size = New System.Drawing.Size(73, 65)
+        Me.B7.TabIndex = 1
+        Me.B7.Text = "7"
+        Me.B7.UseVisualStyleBackColor = True
+        '
+        'B8
+        '
+        Me.B8.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.B8.Location = New System.Drawing.Point(110, 108)
+        Me.B8.Name = "B8"
+        Me.B8.Size = New System.Drawing.Size(74, 65)
+        Me.B8.TabIndex = 2
+        Me.B8.Text = "8"
+        Me.B8.UseVisualStyleBackColor = True
+        '
+        'Bdel
+        '
+        Me.Bdel.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Bdel.Location = New System.Drawing.Point(350, 108)
+        Me.Bdel.Name = "Bdel"
+        Me.Bdel.Size = New System.Drawing.Size(68, 136)
+        Me.Bdel.TabIndex = 4
+        Me.Bdel.Text = "DEL"
+        Me.Bdel.UseVisualStyleBackColor = True
+        '
+        'B9
+        '
+        Me.B9.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.B9.Location = New System.Drawing.Point(191, 108)
+        Me.B9.Name = "B9"
+        Me.B9.Size = New System.Drawing.Size(73, 65)
+        Me.B9.TabIndex = 3
+        Me.B9.Text = "9"
+        Me.B9.UseVisualStyleBackColor = True
+        '
+        'B5
+        '
+        Me.B5.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.B5.Location = New System.Drawing.Point(110, 179)
+        Me.B5.Name = "B5"
+        Me.B5.Size = New System.Drawing.Size(74, 65)
+        Me.B5.TabIndex = 6
+        Me.B5.Text = "5"
+        Me.B5.UseVisualStyleBackColor = True
+        '
+        'B4
+        '
+        Me.B4.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.B4.Location = New System.Drawing.Point(31, 179)
+        Me.B4.Name = "B4"
+        Me.B4.Size = New System.Drawing.Size(73, 65)
+        Me.B4.TabIndex = 5
+        Me.B4.Text = "4"
+        Me.B4.UseVisualStyleBackColor = True
+        '
+        'Bx
+        '
+        Me.Bx.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Bx.Location = New System.Drawing.Point(270, 108)
+        Me.Bx.Name = "Bx"
+        Me.Bx.Size = New System.Drawing.Size(74, 65)
+        Me.Bx.TabIndex = 8
+        Me.Bx.Text = "X"
+        Me.Bx.UseVisualStyleBackColor = True
+        '
+        'B6
+        '
+        Me.B6.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.B6.Location = New System.Drawing.Point(191, 179)
+        Me.B6.Name = "B6"
+        Me.B6.Size = New System.Drawing.Size(73, 65)
+        Me.B6.TabIndex = 7
+        Me.B6.Text = "6"
+        Me.B6.UseVisualStyleBackColor = True
+        '
+        'B2
+        '
+        Me.B2.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.B2.Location = New System.Drawing.Point(110, 250)
+        Me.B2.Name = "B2"
+        Me.B2.Size = New System.Drawing.Size(74, 65)
+        Me.B2.TabIndex = 10
+        Me.B2.Text = "2"
+        Me.B2.UseVisualStyleBackColor = True
+        '
+        'B1
+        '
+        Me.B1.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.B1.Location = New System.Drawing.Point(31, 250)
+        Me.B1.Name = "B1"
+        Me.B1.Size = New System.Drawing.Size(73, 65)
+        Me.B1.TabIndex = 9
+        Me.B1.Text = "1"
+        Me.B1.UseVisualStyleBackColor = True
+        '
+        'Bbagi
+        '
+        Me.Bbagi.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Bbagi.Location = New System.Drawing.Point(270, 179)
+        Me.Bbagi.Name = "Bbagi"
+        Me.Bbagi.Size = New System.Drawing.Size(74, 65)
+        Me.Bbagi.TabIndex = 12
+        Me.Bbagi.Text = "/"
+        Me.Bbagi.UseVisualStyleBackColor = True
+        '
+        'B3
+        '
+        Me.B3.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.B3.Location = New System.Drawing.Point(191, 250)
+        Me.B3.Name = "B3"
+        Me.B3.Size = New System.Drawing.Size(73, 65)
+        Me.B3.TabIndex = 11
+        Me.B3.Text = "3"
+        Me.B3.UseVisualStyleBackColor = True
+        '
+        'Bkurang
+        '
+        Me.Bkurang.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Bkurang.Location = New System.Drawing.Point(270, 250)
+        Me.Bkurang.Name = "Bkurang"
+        Me.Bkurang.Size = New System.Drawing.Size(74, 65)
+        Me.Bkurang.TabIndex = 14
+        Me.Bkurang.Text = "-"
+        Me.Bkurang.UseVisualStyleBackColor = True
+        '
+        'Btambah
+        '
+        Me.Btambah.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btambah.Location = New System.Drawing.Point(270, 321)
+        Me.Btambah.Name = "Btambah"
+        Me.Btambah.Size = New System.Drawing.Size(73, 65)
+        Me.Btambah.TabIndex = 13
+        Me.Btambah.Text = "+"
+        Me.Btambah.UseVisualStyleBackColor = True
+        '
+        'Bsamadengan
+        '
+        Me.Bsamadengan.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Bsamadengan.Location = New System.Drawing.Point(350, 250)
+        Me.Bsamadengan.Name = "Bsamadengan"
+        Me.Bsamadengan.Size = New System.Drawing.Size(68, 136)
+        Me.Bsamadengan.TabIndex = 15
+        Me.Bsamadengan.Text = "="
+        Me.Bsamadengan.UseVisualStyleBackColor = True
+        '
+        'B0
+        '
+        Me.B0.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.B0.Location = New System.Drawing.Point(30, 321)
+        Me.B0.Name = "B0"
+        Me.B0.Size = New System.Drawing.Size(74, 65)
+        Me.B0.TabIndex = 16
+        Me.B0.Text = "0"
+        Me.B0.UseVisualStyleBackColor = True
+        '
+        'BKeluar
+        '
+        Me.BKeluar.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.BKeluar.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BKeluar.ForeColor = System.Drawing.SystemColors.Control
+        Me.BKeluar.Location = New System.Drawing.Point(31, 404)
+        Me.BKeluar.Name = "BKeluar"
+        Me.BKeluar.Size = New System.Drawing.Size(387, 68)
+        Me.BKeluar.TabIndex = 17
+        Me.BKeluar.Text = "Keluar"
+        Me.BKeluar.UseVisualStyleBackColor = False
+        '
+        'BPlusminus
+        '
+        Me.BPlusminus.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BPlusminus.Location = New System.Drawing.Point(109, 321)
+        Me.BPlusminus.Name = "BPlusminus"
+        Me.BPlusminus.Size = New System.Drawing.Size(75, 65)
+        Me.BPlusminus.TabIndex = 18
+        Me.BPlusminus.Text = "+/-"
+        Me.BPlusminus.UseVisualStyleBackColor = True
+        '
+        'Bkoma
+        '
+        Me.Bkoma.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Bkoma.Location = New System.Drawing.Point(191, 321)
+        Me.Bkoma.Name = "Bkoma"
+        Me.Bkoma.Size = New System.Drawing.Size(75, 65)
+        Me.Bkoma.TabIndex = 19
+        Me.Bkoma.Text = "."
+        Me.Bkoma.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.ClientSize = New System.Drawing.Size(438, 492)
+        Me.Controls.Add(Me.Bkoma)
+        Me.Controls.Add(Me.BPlusminus)
+        Me.Controls.Add(Me.BKeluar)
+        Me.Controls.Add(Me.B0)
+        Me.Controls.Add(Me.Bsamadengan)
+        Me.Controls.Add(Me.Bkurang)
+        Me.Controls.Add(Me.Btambah)
+        Me.Controls.Add(Me.Bbagi)
+        Me.Controls.Add(Me.B3)
+        Me.Controls.Add(Me.B2)
+        Me.Controls.Add(Me.B1)
+        Me.Controls.Add(Me.Bx)
+        Me.Controls.Add(Me.B6)
+        Me.Controls.Add(Me.B5)
+        Me.Controls.Add(Me.B4)
+        Me.Controls.Add(Me.Bdel)
+        Me.Controls.Add(Me.B9)
+        Me.Controls.Add(Me.B8)
+        Me.Controls.Add(Me.B7)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Name = "Form1"
+        Me.Text = "Viery Kalkulator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents B7 As Button
+    Friend WithEvents B8 As Button
+    Friend WithEvents Bdel As Button
+    Friend WithEvents B9 As Button
+    Friend WithEvents B5 As Button
+    Friend WithEvents B4 As Button
+    Friend WithEvents Bx As Button
+    Friend WithEvents B6 As Button
+    Friend WithEvents B2 As Button
+    Friend WithEvents B1 As Button
+    Friend WithEvents Bbagi As Button
+    Friend WithEvents B3 As Button
+    Friend WithEvents Bkurang As Button
+    Friend WithEvents Btambah As Button
+    Friend WithEvents Bsamadengan As Button
+    Friend WithEvents B0 As Button
+    Friend WithEvents BKeluar As Button
+    Friend WithEvents BPlusminus As Button
+    Friend WithEvents Bkoma As Button
+End Class
---
+++ Tugas Kalkulator/Kalkulator1/Form1.vb
@@ -0,0 +1,115 @@
+﻿Public Class Form1
+    Dim Angka1, Angka2 As Single
+    Dim lambang As String
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles B7.Click
+        TextBox1.Text = TextBox1.Text + B7.Text
+    End Sub
+
+    Private Sub B1_Click(sender As Object, e As EventArgs) Handles B1.Click
+        TextBox1.Text = TextBox1.Text + B1.Text
+    End Sub
+
+    Private Sub B0_Click(sender As Object, e As EventArgs) Handles B0.Click
+        If TextBox1.Text = "" Then
+            TextBox1.Text = ""
+        Else
+            TextBox1.Text = TextBox1.Text & 0
+        End If
+    End Sub
+
+    Private Sub B2_Click(sender As Object, e As EventArgs) Handles B2.Click
+        TextBox1.Text = TextBox1.Text + B2.Text
+    End Sub
+
+    Private Sub B3_Click(sender As Object, e As EventArgs) Handles B3.Click
+        TextBox1.Text = TextBox1.Text + B3.Text
+    End Sub
+
+    Private Sub B4_Click(sender As Object, e As EventArgs) Handles B4.Click
+        TextBox1.Text = TextBox1.Text + B4.Text
+    End Sub
+
+    Private Sub B5_Click(sender As Object, e As EventArgs) Handles B5.Click
+        TextBox1.Text = TextBox1.Text + B5.Text
+    End Sub
+
+    Private Sub B6_Click(sender As Object, e As EventArgs) Handles B6.Click
+        TextBox1.Text = TextBox1.Text + B6.Text
+    End Sub
+
+    Private Sub B8_Click(sender As Object, e As EventArgs) Handles B8.Click
+        TextBox1.Text = TextBox1.Text + B8.Text
+    End Sub
+
+    Private Sub B9_Click(sender As Object, e As EventArgs) Handles B9.Click
+        TextBox1.Text = TextBox1.Text + B9.Text
+    End Sub
+
+    Private Sub Btambah_Click(sender As Object, e As EventArgs) Handles Btambah.Click
+        If TextBox1.Text = "" Then Exit Sub
+        Angka1 = CSng(TextBox1.Text)
+        lambang = "+"
+        TextBox1.Clear()
+
+    End Sub
+
+    Private Sub Bkurang_Click(sender As Object, e As EventArgs) Handles Bkurang.Click
+        If TextBox1.Text = "" Then Exit Sub
+        Angka1 = CSng(TextBox1.Text)
+        lambang = "-"
+        TextBox1.Clear()
+    End Sub
+
+    Private Sub Bx_Click(sender As Object, e As EventArgs) Handles Bx.Click
+        If TextBox1.Text = "" Then Exit Sub
+        Angka1 = CSng(TextBox1.Text)
+        lambang = "*"
+        TextBox1.Clear()
+    End Sub
+
+    Private Sub Bbagi_Click(sender As Object, e As EventArgs) Handles Bbagi.Click
+        If TextBox1.Text = "" Then Exit Sub
+        Angka1 = CSng(TextBox1.Text)
+        lambang = "/"
+        TextBox1.Clear()
+    End Sub
+
+    Private Sub Bsamadengan_Click(sender As Object, e As EventArgs) Handles Bsamadengan.Click
+        Dim hasil As Single
+        Angka2 = CSng(TextBox1.Text)
+        Select Case lambang
+            Case "+"
+                hasil = Angka1 + Angka2
+            Case "-"
+                hasil = Angka1 - Angka2
+            Case "*"
+                hasil = Angka1 * Angka2
+            Case "/"
+                hasil = Angka1 / Angka2
+        End Select
+        TextBox1.Text = hasil
+    End Sub
+
+    Private Sub Bdel_Click(sender As Object, e As EventArgs) Handles Bdel.Click
+        TextBox1.Text = ""
+    End Sub
+
+    Private Sub BKeluar_Click(sender As Object, e As EventArgs) Handles BKeluar.Click
+        End
+    End Sub
+
+    Private Sub BPlusminus_Click(sender As Object, e As EventArgs) Handles BPlusminus.Click
+        If TextBox1.Text = "" Then Exit Sub
+        If Val(TextBox1.Text) > 0 Then
+            TextBox1.Text = Val(TextBox1.Text) * -1
+        Else
+            TextBox1.Text = Val(TextBox1.Text) * -1
+        End If
+    End Sub
+
+    Private Sub Bkoma_Click(sender As Object, e As EventArgs) Handles Bkoma.Click
+        TextBox1.Text = TextBox1.Text + Bkoma.Text
+
+    End Sub
+End Class
